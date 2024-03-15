4e85303d416e19ba28a2e3bd08f913357d26ce34
王凯 <997284882@qq.com>
2024-03-15 11:59:44 +0800

    修改配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 pluginManagement {
     repositories {
         maven { setUrl("https://maven.aliyun.com/repository/public") }
-        maven { setUrl("https://maven.aliyun.com/repository/public") }
         maven { setUrl("https://maven.aliyun.com/repository/google") }
         maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
         google()
